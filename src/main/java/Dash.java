3cd5e19f5ac501dfb74e90564d3752ecd7a176b4
Sherwin Ee <sherwinee@gmail.com>
2025-01-24 01:35:27 +0800

    Add support for echo

--- src/main/java/Dash.java
+++ src/main/java/Dash.java
@@ -1,12 +1,32 @@
+import java.util.Scanner;
+
 public class Dash {
+    public static void println() {
+        String line = "____________________________________________________________";
+        System.out.println(line);
+    }
     public static void main(String[] args) {
         String botName = "Dash";
         String line = "____________________________________________________________";
-        System.out.println(line);
+        println();
         System.out.println("Hello! I'm " + botName);
         System.out.println("What can I do for you?");
-        System.out.println(line);
+        println();
+
+        Scanner scan = new Scanner(System.in);
+        while (true) {
+            System.out.println();
+            String msg = scan.next();
+            if (msg.equals("bye")) {
+                break;
+            }
+            println();
+            System.out.println(msg);
+            println();
+        }
+
+        println();
         System.out.println("Bye. Hope to see you soon!");
-        System.out.println(line);
+        println();
     }
 }
