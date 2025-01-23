16707540dbac0a7eecc6ee540668b572d7c30bf2
Sherwin Ee <sherwinee@gmail.com>
2025-01-24 01:19:51 +0800

    Rename main class and add greeting

---
+++ src/main/java/Dash.java
@@ -0,0 +1,12 @@
+public class Dash {
+    public static void main(String[] args) {
+        String botName = "Dash";
+        String line = "____________________________________________________________";
+        System.out.println(line);
+        System.out.println("Hello! I'm " + botName);
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you soon!");
+        System.out.println(line);
+    }
+}
