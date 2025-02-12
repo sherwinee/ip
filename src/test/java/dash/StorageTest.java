dc7ac48435914a9b3dbc3a783562a04c165fd563
Sherwin Ee <sherwinee@gmail.com>
2025-02-12 16:56:07 +0800

    Rename method names in tests

--- src/test/java/dash/StorageTest.java
+++ src/test/java/dash/StorageTest.java
@@ -12,31 +12,31 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class StorageTest {
     @Test
-    public void getTaskFromString_correctFormat_success() {
+    public void parseTaskFromString_correctFormat_success() {
         String testString1 = "T | 0 | Buy groceries";
-        assertEquals(new Todo("Buy groceries").toString(), Storage.getTaskFromString(testString1).toString());
+        assertEquals(new Todo("Buy groceries").toString(), Storage.parseTaskFromString(testString1).toString());
 
         String testString2 = "E | 0 | Career fair | 2025-01-20 | 2025-01-21";
         assertEquals(new Event("Career fair", LocalDate.parse("2025-01-20"), LocalDate.parse("2025-01-21")).toString(),
-                Storage.getTaskFromString(testString2).toString());
+                Storage.parseTaskFromString(testString2).toString());
     }
 
     @Test
-    public void getTaskFromString_invalidFormat_failure() {
+    public void parseTaskFromString_invalidFormat_failure() {
         String testString = "E || test todo";
-        assertThrows(IllegalArgumentException.class, () -> Storage.getTaskFromString(testString));
+        assertThrows(IllegalArgumentException.class, () -> Storage.parseTaskFromString(testString));
 
         String testString2 = "G | 1 | invalid event";
-        assertThrows(IllegalArgumentException.class, () -> Storage.getTaskFromString(testString));
+        assertThrows(IllegalArgumentException.class, () -> Storage.parseTaskFromString(testString));
     }
 
     @Test
-    public void getTaskListString_withTasks_success() {
+    public void getFormattedTasksString_withTasks_success() {
         Event testEvent = new Event("Career fair", LocalDate.parse("2025-01-20"), LocalDate.parse("2025-01-21"));
         Todo testTodo = new Todo("test todo");
         TaskList tasks = new TaskList();
         tasks.add(testEvent);
         Storage storage = new Storage("./data/TestDash.txt", tasks);
-        assertEquals("E | 0 | Career fair | 2025-01-20 | 2025-01-21", storage.getTaskListString());
+        assertEquals("E | 0 | Career fair | 2025-01-20 | 2025-01-21", storage.getFormattedTasksString());
     }
 }
