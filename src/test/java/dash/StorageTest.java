4cf259557a0196d83786ebef1f413280d18af561
Sherwin Ee <sherwinee@gmail.com>
2025-02-23 16:43:13 +0800

    Update StorageTest

--- src/test/java/dash/StorageTest.java
+++ src/test/java/dash/StorageTest.java
@@ -6,6 +6,7 @@ import dash.task.Todo;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -13,11 +14,11 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class StorageTest {
     @Test
     public void parseTaskFromString_correctFormat_success() {
-        String testString1 = "T | 0 | Buy groceries";
-        assertEquals(new Todo("Buy groceries").toString(), Storage.parseTaskFromString(testString1).toString());
+        String testString1 = "T|0|Buy groceries|";
+        assertEquals(new Todo("Buy groceries", List.<String>of()).toString(), Storage.parseTaskFromString(testString1).toString());
 
-        String testString2 = "E | 0 | Career fair | 2025-01-20 | 2025-01-21";
-        assertEquals(new Event("Career fair", LocalDate.parse("2025-01-20"), LocalDate.parse("2025-01-21")).toString(),
+        String testString2 = "E|0|Career fair|2025-01-20|2025-01-21|";
+        assertEquals(new Event("Career fair", List.<String>of(), LocalDate.parse("2025-01-20"), LocalDate.parse("2025-01-21")).toString(),
                 Storage.parseTaskFromString(testString2).toString());
     }
 
@@ -32,11 +33,10 @@ public class StorageTest {
 
     @Test
     public void getFormattedTasksString_withTasks_success() {
-        Event testEvent = new Event("Career fair", LocalDate.parse("2025-01-20"), LocalDate.parse("2025-01-21"));
-        Todo testTodo = new Todo("test todo");
+        Event testEvent = new Event("Career fair", List.<String>of(), LocalDate.parse("2025-01-20"), LocalDate.parse("2025-01-21"));
         TaskList tasks = new TaskList();
         tasks.add(testEvent);
         Storage storage = new Storage("./data/TestDash.txt", tasks);
-        assertEquals("E | 0 | Career fair | 2025-01-20 | 2025-01-21", storage.getFormattedTasksString());
+        assertEquals("E|0|Career fair|2025-01-20|2025-01-21|", storage.getFormattedTasksString());
     }
 }
